cc45b99e22dfeda4f8652f53902ca6e898b04c51
Riina Hemmilä <riina@Velhomestari-Airi.local>
2023-01-09 23:06:54 +0200

    added item_counter class, enchantment list print and counters

--- main.py
+++ main.py
@@ -1,9 +1,42 @@
-class Enchnantment:
+# class with information from enchantments.txt.
+class Enchantment:
 	
 	def __init__(self, enchantment_name, max_level, description):
 		self.enchantment_name = enchantment_name
 		self.description = description
 		self.max_level = max_level
+
+	def increase_level():
+		if level < self.max_level:
+			level += 1
+		else:
+			print("Level can't be increased.")
+
+	def decrease_level():
+		if 1 < level < self.max_level:
+			level -= 1
+		else:
+			print("Level can't be decreased.")
+
+	def view_description():
+		print(self.description)
+
+
+# class to count amount of enchantment books.
+class Item_counter(Enchantment):
+
+	def __init__(self, amount):
+		Enchantment.__init(self, enchantment_name, max_level, description)
+		self.amount = 0
+
+	def increase_amount():
+		self.amount += 1
+
+	def decrease_amount():
+		if self.amount > 0:
+			self.amount -= 1
+		else:
+			print("Amount can't be decreased.")
 
 
 # creating an empty dictionary.
@@ -19,12 +52,18 @@ for line in enchantments_file.readlines():
 	else:
 		row = line.split(":")
 		enchantment_name, max_level, description = row
-		enchantment_with_attributes = Enchnantment(enchantment_name, max_level, description)
+		enchantment_with_attributes = Enchantment(enchantment_name, max_level, description)
 		enchantments_dict[enchantment_name] = enchantment_with_attributes
 
 
 # close the file
 enchantments_file.close()
 
+# print all possible enchantments.
 for enchantment_key in enchantments_dict:
-	print(enchantment_key, enchantments_dict[enchantment_key].max_level)
+	for enchantment_level in range(int(enchantments_dict[enchantment_key].max_level)):
+		if int(enchantments_dict[enchantment_key].max_level) == 1:
+			print(enchantment_key)
+		else:
+			print(enchantment_key, enchantment_level+1)
+
